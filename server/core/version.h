a62495205a4165d015fb2d50092556d299250357
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-07-29 02:31:22 -0300

    v1.5.0: eliminar modal connect-dialog, auto-connect inmediato, inline reconnect UI
    Cambios principales:
    
    - Eliminado el modal de conexion (connect-dialog) que requeria clickear Connect
    - Auto-connect inmediato al cargar la pagina: ws://hostname:8080
    - En caso de desconexion, auto-reconnect silencioso a los 3 segundos
    - Boton inline "Reconnect" en la barra superior (solo visible en error)
    - scheduleReconnect() con guard para evitar stacking de timeouts
    - Fix: CSS specificity bug que impedia mostrar el boton reconnect
    
    Version bump: 1.4.2 -> 1.5.0 (cambio mayor en UX)

--- server/core/version.h
+++ server/core/version.h
@@ -19,14 +19,14 @@
  * Version menor. Se incrementa con nuevas funcionalidades
  * compatibles hacia atras.
  */
-#define LOGIC_VERSION_MINOR 4
+#define LOGIC_VERSION_MINOR 5
 
 /**
  * @def LOGIC_VERSION_PATCH
  * Patch level. Se incrementa con correcciones de bugs
  * o mejoras menores de rendimiento.
  */
-#define LOGIC_VERSION_PATCH 1
+#define LOGIC_VERSION_PATCH 0
 
 /**
  * @def LOGIC_VERSION_STRING
@@ -34,6 +34,6 @@
  * y en los mensajes de log de inicio.
  * Formato: "MAJOR.MINOR.PATCH"
  */
-#define LOGIC_VERSION_STRING "1.4.1"
+#define LOGIC_VERSION_STRING "1.5.0"
 
 #endif // LOGIC_VERSION_H
